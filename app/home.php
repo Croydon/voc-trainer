5bd2d92714c4c66526121f4d3a94c92a14c3ada0
Croydon <git@rvapps.org>
2015-04-21 21:26:33 +0200

    UI: Adjust login form

--- app/home.php
+++ app/home.php
@@ -1,11 +1,18 @@
 <?php
 	if(!isset($f3)) { http_response_code(404); exit; }
 	
-	$f3->set("page", "home.htm");
-	$f3->set("title", "Welcome");
-	$f3->set("headline", "Welcome to your personal Voc Trainer");
-
-	//$f3->set("users", $f3->db->exec("SELECT username FROM ". $f3->get("prefix") ."user"));
-
+	if($f3->get("loggedIn") == true)
+	{
+		$f3->set("title", "Welcome");
+		$f3->set("page", "home.htm");
+		$f3->set("headline", "Welcome to your personal Voc Trainer");
+		$f3->set("users", $f3->db->exec("SELECT username FROM ". $f3->get("prefix") ."user"));
+	}
+	{
+		$f3->set("title", "Welcome · Login");
+		$f3->set("page", "login.htm");
+		$f3->set("headline", "User area");
+	}
+	
 	echo View::instance()->render("layout.htm");
 ?>
